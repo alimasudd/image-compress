c31d6219558dd07fa3c39ad12a11a0e2cfbdf555
alim <alimasudd.dev@gmail.com>
2023-08-18 09:41:30 +0700

    delete R layout and add deprecation warning

--- app/src/main/java/id/alimasudd/image_compress/MainActivity.kt
+++ app/src/main/java/id/alimasudd/image_compress/MainActivity.kt
@@ -1,3 +1,5 @@
+@file:Suppress("DEPRECATION")
+
 package id.alimasudd.image_compress
 
 import android.annotation.SuppressLint
@@ -9,7 +11,6 @@ import android.os.Bundle
 import android.os.Environment
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import id.alimasudd.image_compress.R
 import id.alimasudd.image_compress.databinding.ActivityMainBinding
 import id.alimasudd.image_compress.utils.CompressImage
 import id.alimasudd.image_compress.utils.FileUtil.from
